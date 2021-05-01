993c2512ffe7b907acf10859c9b33b8fcf26cb0b
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-01 13:51:51 -0400

    add .json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from "react";
+import Main from "./components/Main";
+import employees from "./employees.json";
+import Wrapper from "./components/Wrapper";
+
+
+class App extends Component {
+  state = {
+    employees
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Jumbotron />
+        <Main />
+        </Wrapper>
+    )
+  }
+};
 
 export default App;
---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../Search";
+
+function Main() {
+    return (
+        <div>
+            <Search />
+            </div>
+    )
+}
+
+export defualt Main;
