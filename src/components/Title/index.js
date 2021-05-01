5a3e3012c72bc30eb3648cc222d6eb60e2701e7f
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-01 14:39:30 -0400

    adjust app.js

---
+++ src/components/Title/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Jumbotron } from "reactsstrap";
+import "./style.css";
+
+function Title() {
+    return (
+        <div>
+            <Jumbotron clasName="jumbotron">
+                <h1 className="title">TBD</h1>
+                <p className="lead">Search by name, position or location!</p>
+
+
+            </Jumbotron>
+            </div>
+    );
+    }
+    export default Title;
